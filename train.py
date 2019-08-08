8e1cd8ed4b6bde0f57e2f1f35cdf2631fe16d345
Prabhu204 <prabhukumarreddy204@gmail.com>
2019-08-08 12:00:16 +0200

    update

--- train.py
+++ train.py
@@ -31,21 +31,24 @@ def get_args():
     parser.add_argument('-es', '--early_stopping', type=int, default= 6, help="""early stopping is used to stop training of network, 
                                                                         if does not improve validation loss""")
     parser.add_argument('-i', '--imagesize', type=int, default=224, help="it is used to resize the image pixels" )
-    parser.add_argument('-lr', '--lr', type=int, default=0.01)
+    parser.add_argument('-lr', '--lr', type=int, default=0.001)
     args = parser.parse_args()
     return args
 
 def train(opt):
-    global best_valLoss
     traindata, trainGenerator, classes = preprocess(path='./data'+os.sep+opt.train, batchsize=opt.batchsize,
                                                     imagesize=opt.imagesize, shuffle=True)
     valdata, validationGenerator, classes = preprocess(path='./data'+os.sep+opt.val, batchsize=opt.batchsize,
-                                                      imagesize=opt.imagesize, shuffle=False)
+                                                      imagesize=opt.imagesize, shuffle=True)
     # print(iter(trainGenerator).__next__())
 
     num_channels = iter(trainGenerator).__next__()[0].size()[1]
-    path_t = 'results/VdcnnIR_train_{}.txt'.format(opt.depth)
-    path_v = 'results/VdcnnIR_val_{}.txt'.format(opt.depth)
+    if opt.conv1_1 and opt.depth == 16:
+        path_t = 'results/VdcnnIR_train_C11_{}.txt'.format(opt.depth)
+        path_v = 'results/VdcnnIR_val_C11_{}.txt'.format(opt.depth)
+    else:
+        path_t = 'results/VdcnnIR_train_{}.txt'.format(opt.depth)
+        path_v = 'results/VdcnnIR_val_{}.txt'.format(opt.depth)
     if os.path.exists(path_t):
         os.remove(path_t)
         os.mknod(path_t)
@@ -57,9 +60,15 @@ def train(opt):
     else:
         os.mknod(path_v)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = Vgg(num_channels=num_channels,num_classes=classes,depth=opt.depth, initialize_weights=True,
+    if opt.conv1_1 and opt.depth==16:
+        model = Vgg(num_channels=num_channels,num_classes=classes,depth=opt.depth, initialize_weights=True,
                 conv1_1=opt.conv1_1).to(device)
-    optimizer = optim.SGD(model.parameters(), lr=opt.lr, momentum=0.9)
+
+    else:
+        model = Vgg(num_channels=num_channels, num_classes=classes, depth=opt.depth, initialize_weights=True,
+                    conv1_1=False).to(device)
+
+    optimizer = optim.Adam(model.parameters(), lr=opt.lr)
     criterion = nn.CrossEntropyLoss()
 
     def plot_fig(train_loss, val_loss):
@@ -70,7 +79,11 @@ def train(opt):
         plt.xlabel("Epochs")
         plt.ylabel("Loss")
         plt.legend()
-        plt.savefig("figures/trainVal_loss_{}.png".format(opt.depth))
+        if opt.conv1_1 and opt.depth==16:
+            plt.savefig("figures/trainVal_loss_C11_{}.png".format(opt.depth))
+        else:
+            plt.savefig("figures/trainVal_loss_{}.png".format(opt.depth))
+
         # plt.show()
         # plt.close()
         return None
@@ -102,7 +115,7 @@ def train(opt):
             optimizer.step()
             total_predictions.extend(prob_)
             total_labels.extend(label.cpu())
-            print('Iter[{}/{}]\tEpoch[{}/{}]\tLoss{}\tacc{}'.format(idx+1, len(trainGenerator), epoch+1, opt.epochs,
+            print('Iter: [{}/{}]\t Epoch: [{}/{}]\t Loss: {}\t Acc: {}'.format(idx+1, len(trainGenerator), epoch+1, opt.epochs,
                                                                     loss.item(),
                                                                     metrics.accuracy_score(label.cpu(), prob_)))
 
@@ -128,29 +141,28 @@ def train(opt):
                 val_loss.append(loss_v.item()*len(label_e.cpu()))
                 total_ValLabels.extend(label_e.cpu())
                 total_Valpredictions.extend(pred_e)
-                print('Iter[{}/{}]\tEpoch[{}/{}]\tLoss{}\tacc{}'.format(idx_e + 1, len(validationGenerator), epoch + 1,
+                print('Iter: [{}/{}]\t Epoch: [{}/{}]\t Loss: {}\t Acc: {}'.format(idx_e + 1, len(validationGenerator), epoch + 1,
                                                                         opt.epochs, loss_v.item(),
                                                                         metrics.accuracy_score(label_e.cpu(), pred_e)))
         val_lossEpoch= sum(val_loss)/len(valdata)
         totalVal_loss.append(val_lossEpoch)
         with open(path_v, 'a') as f:
-            f.write('Epoch{}\tLoss{}\tAccuracy{}\n'.format(epoch+1, val_lossEpoch,
+            f.write('Epoch: {}\tLoss: {}\tAccuracy: {}\n'.format(epoch+1, val_lossEpoch,
                                                          metrics.accuracy_score(total_ValLabels,total_Valpredictions)))
-        flag_, best_valLoss = early_stopping(val_loss=loss_v, model=model)
         # roc_fig = scikitplot.metrics.plot_roc(total_ValLabels, total_Valpredictions, figsize=(12, 12))
         # plt.savefig('figures/ROC_{}.png'.format(opt.depth))
         # plt.show()
         plot_fig(train_loss=totalTrain_loss, val_loss=totalVal_loss)
         # print(loss_fig)
         if best_score is None:
-            best_score = val_loss
+            best_score = val_lossEpoch
             torch.save(model, 'models/VdcnnIR_{}'.format(opt.depth))
-        elif val_loss > best_score:
+        elif val_lossEpoch > best_score:
             print("Loss:{} doesn't decreased from {}".format(val_loss, best_score))
             count +=1
             if count >= opt.early_stopping:
                 early_stop = True
-        elif val_loss < best_score:
+        elif val_lossEpoch < best_score:
             print("Loss:{} decreased from {}. Saving model........".format(val_loss, best_score))
             best_score = val_loss
             torch.save(model, 'models/VdcnnIR_{}'.format(opt.depth))
@@ -159,9 +171,13 @@ def train(opt):
             break
         model.train()
     losses = {'trainLoss':totalTrain_loss, 'valLoss':totalVal_loss}
-    with open('results/losses_{}'.format(opt.depth), 'wb') as f:
-        pickle.dump(losses, f)
-    return best_valLoss
+    if opt.conv1_1 and opt.depth == 16:
+        with open('results/losses_C11_{}'.format(opt.depth), 'wb') as f:
+            pickle.dump(losses, f)
+    else:
+        with open('results/losses_{}'.format(opt.depth), 'wb') as f:
+            pickle.dump(losses, f)
+    return best_score
 
 if __name__ == '__main__':
     opt = get_args()
